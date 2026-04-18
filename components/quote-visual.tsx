4a89a7577b6d73a03c8ff5fe2df3928d71df0d9b
krishna <km3753582@gmail.com>
2026-04-19 03:11:21 +0530

    Swap quote visual details

--- components/quote-visual.tsx
+++ components/quote-visual.tsx
@@ -8,12 +8,15 @@ export function QuoteVisual() {
             <span className="quote-body" />
             <span className="quote-arm quote-arm-left" />
             <span className="quote-arm quote-arm-right" />
+            <span className="quote-cup quote-cup-left" />
           </div>
           <div className="quote-person quote-person-right">
             <span className="quote-head" />
             <span className="quote-body" />
             <span className="quote-arm quote-arm-left" />
             <span className="quote-arm quote-arm-right" />
+            <span className="quote-laptop" />
+            <span className="quote-cup quote-cup-right" />
           </div>
         </div>
         <div className="quote-table" />
